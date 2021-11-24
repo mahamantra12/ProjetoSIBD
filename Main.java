2aef01bc80df2b28f858a0b7c18f3d0129bdeab3
Maha Mantra <mahamantra@alu.ufc.br>
2021-11-24 20:48:51 -0300

    Teste2

--- Main.java
+++ Main.java
@@ -1,35 +1,11 @@
 
+public class Main{
 
-/**
- * Escreva uma descrição da classe Main aqui.
- * 
- * @author (seu nome) 
- * @version (um número da versão ou uma data)
- */
-public class Main
-{
-    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
-    private int x;
-
-    /**
-     * Construtor para objetos da classe Main
-     */
     public Main()
     {
-        // inicializa variáveis de instância
-        x = 0;
+        
     }
 
-    /**
-     * Um exemplo de um método - substitua este comentário pelo seu próprio
-     * 
-     * @param  y   um exemplo de um parâmetro de método
-     * @return     a soma de x e y 
-     */
-    public int sampleMethod(int y)
-    {
-        // escreva seu código aqui
-        return x + y;
-    }
+   
 
 }
